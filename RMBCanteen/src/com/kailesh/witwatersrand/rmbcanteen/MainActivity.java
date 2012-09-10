a16b824ea085976a0ae9e9fb79204320a9496ad0
Kailesh Ramjee <kaileshramjee@gmail.com>
2012-09-10 15:20:41 +0200

    Understand how buttons can be used displaying text when clicking a button

--- RMBCanteen/src/com/kailesh/witwatersrand/rmbcanteen/MainActivity.java
+++ RMBCanteen/src/com/kailesh/witwatersrand/rmbcanteen/MainActivity.java
@@ -3,18 +3,28 @@ package com.kailesh.witwatersrand.rmbcanteen;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		Button button = (Button) findViewById(R.id.clickMeButton);
+		button.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				Toast.makeText(MainActivity.this, "Button Clicked!", Toast.LENGTH_SHORT).show();
+			}
+		});   
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
 }
